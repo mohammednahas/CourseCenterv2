27c8c6ba5bf00f7a713a2f81f98ad3e1c0f8600e
mohammed elnahas <m.moustafa.elnahas@gmail.com>
2026-09-26 14:29:15 +0300

    Add gitignore

--- Core/CourseCenterv2.Domain/obj/Debug/net10.0/CourseCenterv2.Domain.AssemblyInfo.cs
+++ Core/CourseCenterv2.Domain/obj/Debug/net10.0/CourseCenterv2.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseCenterv2.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b0d489ccb7c3f2f8cacc6d8ba6ea33762743575")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a376cb3cc9e4ecf5306acca2078091697062696")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseCenterv2.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseCenterv2.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
